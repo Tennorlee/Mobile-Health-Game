498618e37f6fec7c1cc6f4fed2a7d28ef0e5f023
Tennorlee <tennorlee@gmail.com>
2019-02-27 14:45:24 +0800

    big changes on UI, added Equipment bar, description, quick slot

---
+++ Assets/InventorySystem01/Assets/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour
+{
+    private const int SLOT = 18;
+    private List<Item> itemsList = new List<Item>();
+    
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+
+    public void AddItem(Item item)
+    {
+        if (itemsList.Count < SLOT)
+        {
+            if 
+            
+        };
+    }
+}
---
+++ Assets/InventorySystem01/Assets/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour {
+
+    public string name { get; set; }
+    public Sprite icon { get; set; }
+    public string description { get; set; }
+    public enum Type { equip, consumables, throwable};
+    public Type type;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        transform.parent.parent.parent.parent.GetComponent<BigCanvaController>().selectedItem = this.transform;
+    }
+    
+}
